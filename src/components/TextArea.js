382298cd0614a8c57a15d8859eee052ea715ee99
yaeltuv <yaelt@codefuel.com>
2023-01-31 11:43:11 +0200

    css fixes

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -13,7 +13,7 @@ export default function TextArea({placeholder, value, setValue, label, required}
             value={value}
             placeholder={placeholder}
             onChange={handleChange}
-            style={{ width: 270, height: 150, outline: 'none', border: '1px solid #CCCCCC', borderRadius: 5 }}
+            style={{ width: 262, height: 150, outline: 'none', border: '1px solid #CCCCCC', borderRadius: 5 , padding: 5}}
         />
         </div>
     );
